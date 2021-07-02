902f98af1497249ea3c41363a3de36cc2fe49032
mukul-rana <56799682+mukul-rana@users.noreply.github.com>
2021-07-02 16:59:45 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+# Telegram-Bot
+Telegram bot helps us to connect to our pc using telegram from any device and perform some tasks like a TODO list, Curi list and obtaining screenshot using commands in chat.
+
+## Features 
+* Obtain screenshot of current window open on pc
+* Add and delete tasks in ToDo list 
+* Add and delete queries in Curi List (list of questions born from curiosity )
+
+##  Installation 
+1. Telegram Bot requires [Python 3] to run.
+2. Obtain Telegram api_id and api_hash from [Telegram api].
+3. Create a channel on telegram and obtain its link.
+4. Install follpython modules using command prompt.
+`pip install telethon `
+`pip install pyautogui`
+5. Enter your `api_id` , `api_hash`, `username` , `phone_number` and `channel link` in telegram.py:
+```
+api_id = ******
+api_hash = '**********************'
+username = '********'
+phone= '+91*********'
+user_input_channel = 'https://t.me/***********'
+```
+6. If you are first time user, then it authenticates by using OTP and a `.session` file is created in the directory after successful authentication. There is no need to re-authenticate until the `.session` is available in the directory
+
+## How to use
+* First enter the reset time for application to recheck latest commands (in seconds).
+* Enter the time for which you want to run the application (in minutes).
+* Enter the command you wanna run for following tasks:
+    * Screenshot : `screen`  
+    * Todo List : 
+        * `task` for viewing tasks
+        * `task add : Any task you wanna add` for adding tasks
+        * `task delete : index number of task you wanna delete` for deleting task ( you can see index number by running task command)
+    * Curi List:
+        * `curi` for viewing tasks
+        * `curi add : Any Question you wanna add` for adding tasks
+        * `curi delete : index number of question you wanna delete` for deleting question ( you can see index number by running curi command)
+     > Note: The above commands are case-insensitive i.e, you can enter command in any case but the query or task you are going to add is in the same case as you are entering.
+
+[Telegram api]:<https://core.telegram.org/api/obtaining_api_id>
+[Python 3]:<https://www.python.org/downloads/>
+
+
+
+
+
+
+
---
+++ telegram.py
@@ -0,0 +1,193 @@
+from itertools import count
+import telethon.sync
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.messages import (GetHistoryRequest)
+import time
+from telethon.tl.types import (
+PeerChannel
+)
+import pyautogui
+
+api_id = ******
+api_hash = '*****************'
+username = '*********'
+phone= '+***********)'
+user_input_channel = 'https://t.me/**********'
+
+
+
+#input("enter entity(telegram URL or entity id):")
+
+client = TelegramClient(username, api_id, api_hash)
+client.start()
+print("Client Created")
+# Ensure you're authorized
+if not client.is_user_authorized():
+	client.send_code_request(phone)
+	try:
+		client.sign_in(phone, input('Enter the code: '))
+	except SessionPasswordNeededError:
+		client.sign_in(password=input('Password: '))
+
+
+
+
+
+if user_input_channel.isdigit():
+	entity = PeerChannel(int(user_input_channel))
+else:
+	entity = user_input_channel
+
+my_channel = client.get_entity(entity)
+
+#client.send_message(entity=entity,message="I am not faded")
+
+
+
+n = float(input('Enter the reset time in seconds : '))
+l = float(input('Enter the time period (in minutes) : '))
+l = (l*60)/n
+print('Program will run ' + str(l) +' times')
+while(l>0):
+	
+	time.sleep(n)
+	l-=1
+	all_messages = [] # contains all messages with details in range 100 
+	while True:   
+		history = client(GetHistoryRequest(
+			peer=my_channel,
+			offset_id=0,
+			offset_date=None,
+			add_offset=0,
+			limit=25,
+			max_id=0,
+			min_id=0,
+			hash=0
+		))
+
+
+		if not history.messages:
+			break
+		messages = history.messages
+
+		for message in messages:
+			all_messages.append(message.to_dict())
+		break
+
+
+	messages =[]
+	for i in all_messages:
+		if i['message'] == 'Anything else boss':
+			break
+		messages.append(i['message'].lower())
+	messages.reverse()
+
+
+
+
+
+
+	for i in range(len(messages)):
+		message = messages[i].split(':')
+		print('.'+message[0].strip()+'.')
+		if(message[0].strip() =='screen'):
+			upd = pyautogui.screenshot()
+			upd.save('files\\updates.png')
+			client.send_file(my_channel, 'files\\updates.png', caption="Your Screen Update is ready Sir")
+			
+
+		elif(message[0].strip() == 'curi add'):
+			
+			file = 'files\\curi.txt'
+			client.send_message(entity = entity, message = 'Query < ' + message[1] + ' > addedd boss')
+			with open(file) as file_object:
+				lines = file_object.readlines()
+			
+			count = int(lines[0].split(" ")[0])
+
+			with open(file, 'w') as filo:
+				filo.write( str(count+1) + ' \n')
+				for i in range(1,len(lines)):
+					filo.write( lines[i])
+				filo.write( str(count+1) + ' ' + message[1].strip()+'\n')
+
+		elif(message[0].strip() == 'curi'):
+			file = open("files\\curi.txt","r")
+			lines = ''.join(file.readlines()[1:]).strip()
+			client.send_message(entity=entity,message=lines)
+			
+
+		elif(message[0].strip() == 'curi delete'):
+			file = "files\\curi.txt"
+			delNum = message[1].strip()
+			with open("files\\curi.txt", "r") as f:
+				lines = f.readlines()
+			
+			count = int(lines[0].split(" ")[0])
+			if count < int(delNum):
+				client.send_message(entity=entity,message="No such element exist. Try again")
+			else:
+				delNum = int(delNum)
+				with open(file, 'w') as filo:
+					filo.write( str(count-1) + ' \n')
+					for i in range(1,delNum):
+						filo.write( lines[i])
+					for i in range(delNum+1,len(lines)):
+						filo.write( str(i-1) + ' ' +  ' '.join(lines[i].split(' ')[1:]))
+				
+				client.send_message(entity=entity,message="Curi <  "+ lines[delNum][:-1] + " >  deleted boss")
+
+
+
+
+
+		elif(message[0].strip() == 'task add'):
+			file = 'files\\todo.txt'
+			client.send_message(entity = entity, message = 'Task < ' + message[1] + ' > added boss')
+			with open(file) as file_object:
+				lines = file_object.readlines()
+			
+			count = int(lines[0].split(" ")[0])
+
+			with open(file, 'w') as filo:
+				filo.write( str(count+1) + ' \n')
+				for i in range(1,len(lines)):
+					filo.write( lines[i])
+				filo.write( str(count+1) + ' ' + message[1].strip()+'\n')
+
+		elif(message[0].strip() == 'task'):
+			file = open("files\\todo.txt","r")
+			lines = ''.join(file.readlines()[1:]).strip()
+			client.send_message(entity=entity,message=lines)
+			
+
+		elif(message[0].strip() == 'task delete'):
+			file = "files\\todo.txt"
+			delNum = message[1].strip()
+			with open("files\\todo.txt", "r") as f:
+				lines = f.readlines()
+			
+			count = int(lines[0].split(" ")[0])
+			if count < int(delNum):
+				client.send_message(entity=entity,message="No such element exist. Try again")
+			else:
+				delNum = int(delNum)
+				with open(file, 'w') as filo:
+					filo.write( str(count-1) + ' \n')
+					for i in range(1,delNum):
+						filo.write( lines[i])
+					for i in range(delNum+1,len(lines)):
+						filo.write( str(i-1) + ' ' +  ' '.join(lines[i].split(' ')[1:]))
+				
+				client.send_message(entity=entity,message=" Task <  "+ lines[delNum][:-1] + " >  deleted boss")
+
+
+		else:
+			client.send_message(entity=entity,message="Boss I am unable to find < " + message[0].strip() + ' >')
+
+
+	if messages != []:
+		print(".Anything else boss.")
+
+		client.send_message(entity=entity,message="Anything else boss")
